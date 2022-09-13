842d810271265a1a3a0253e9492fde5d5d825dde
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-09-13 14:15:59 +0700

    Tugas Mid Praktek

---
+++ 1. Python Fundamental for Data Science/3. Operator Matematika dan Pembanding/MidPraktek.py
@@ -0,0 +1,15 @@
+# Tugas Mid Praktek
+# Buatlah sebuah program yang dapat menentukan suatu variabel apakah merupakan bilangan genap atau ganjil. Lakukan percobaan dengan langkah berikut:
+
+# Buat variabel dengan nama “angka” yang diisi dengan nilai 10
+# Ganti bagian … dengan perhitungan untuk menentukan angka modulus 2 bernilai 0.
+# Jika angka bernilai genap maka keluarkan "angka termasuk bilangan genap". Selanjutnya, ganti nilai variable angka dengan nilai 5 dan ceklah kembali apakah angka bernilai ganjil. Jika iya, keluarkan "angka termasuk bilangan ganjil".
+
+# Note: Kamu bisa menggunakan materi-materi sebelumnya dengan menggabungkannya di tugas praktik ini untuk membantu kamu dalam mengerjakannya.
+
+angka=5
+
+if(angka % 2==0):
+    print("angka termasuk bilangan genap")
+else:
+    print("angka termasuk bilangan ganjil")
