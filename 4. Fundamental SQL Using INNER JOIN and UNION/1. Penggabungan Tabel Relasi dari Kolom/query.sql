782a4c60bb7b73694fb4a759599f7f3f984ae61f
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-09-21 15:15:35 +0700

    Create Query

--- 4. Fundamental SQL Using INNER JOIN and UNION/1. Penggabungan Tabel Relasi dari Kolom/query.sql
+++ 4. Fundamental SQL Using INNER JOIN and UNION/1. Penggabungan Tabel Relasi dari Kolom/query.sql
@@ -12,3 +12,13 @@ SELECT * FROM ms_item_warna, ms_item_kategori
 WHERE nama_barang = nama_item;
 -- Terlihat jumlah data yang dihasilkan tetap 6 baris data, namun dengan urutan kolom yang berbeda.
 -- Dimana dua kolom pertama adalah dari tabel ms_item_warna, dan dua kolom berikutnya dari tabel ms_item_kategori. Hal ini sesuai dengan urutan nama tabel yang diketikkan setelah FROM.
+
+-- ! Menggunakan Prefix Nama Tabel !
+-- Sebagai contoh, jika akan menggabungkan kedua tabel, dengan menyatakan tabel ms_item_warna terlebih dahulu di bagian FROM, tetapi yang ingin dimunculkan kolomnya di awal hasil adalah dari yang ms_item_kategori, maka querynya akan menjadi sebagai berikut.
+SELECT ms_item_kategori.*,  ms_item_warna.* 
+FROM ms_item_warna, ms_item_kategori 
+WHERE nama_barang = nama_item;
+-- Terlihat urutan kolom yang muncul adalah dari tabel ms_item_kategori terlebih dahulu.
+
+-- ! Penggabungan Tanpa Kondisi !
+SELECT * FROM ms_item_kategori, ms_item_warna;
