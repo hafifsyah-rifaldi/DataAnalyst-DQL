48ad2cb07b1a25017fe452bedd28a2673a4c26a0
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-09-26 14:11:17 +0700

    Create Query

---
+++ 4. Fundamental SQL Using INNER JOIN and UNION/3. UNION/query.sql
@@ -0,0 +1,7 @@
+-- ! Tabel yang Akan Digabungkan !SELECT * FROM tabel_A;
+SELECT * FROM tabel_A;
+SELECT * FROM tabel_B;
+-- Sesuai dengan syarat untuk penggabungan dengan UNION yang telah dijelaskan tadi bahwa:
+-- jumlah kolom tabel_A dan tabel_B adalah sama
+-- kolom-kolom pada tabel_A dan tabel_B memiliki tipe data yang sama, dan
+-- kolom-kolom pada tabel_A dan tabel_B memiliki urutan posisi yang sama.
