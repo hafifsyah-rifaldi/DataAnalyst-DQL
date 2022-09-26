870d5493d022a3674dbd3112a70e123da9e0fedc
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-09-26 14:13:03 +0700

    Create Query

--- 4. Fundamental SQL Using INNER JOIN and UNION/3. UNION/query.sql
+++ 4. Fundamental SQL Using INNER JOIN and UNION/3. UNION/query.sql
@@ -5,3 +5,10 @@ SELECT * FROM tabel_B;
 -- jumlah kolom tabel_A dan tabel_B adalah sama
 -- kolom-kolom pada tabel_A dan tabel_B memiliki tipe data yang sama, dan
 -- kolom-kolom pada tabel_A dan tabel_B memiliki urutan posisi yang sama.
+
+
+-- ! Menggunakan UNION !
+-- Kedua tabel_A dan tabel_B sudah memiliki jumlah kolom yang sama, dan juga urutan posisi kolom juga sama, jadi bisa langsung menggabungkan kedua kolom tersebut dengan menambahkan UNION. 
+SELECT * FROM tabel_A
+UNION
+SELECT * FROM tabel_B;
